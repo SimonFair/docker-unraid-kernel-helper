db78196b5a91dec020045fd93fa6e0813b0eb772
ich777 <ich777@noreply.example.org>
2020-05-26 19:31:13 +0200

    added nVidia build warning

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -55,6 +55,19 @@ echo "---container is using it could ruin the whole build---"
 echo "------process. The build process begins after 60------"
 echo "------seconds that this message first appeared--------"
 echo "------------------------------------------------------"
+if [ "${BUILD_NVIDIA}" == "true" ]; then
+echo
+echo "------------------------------------------------------"
+echo "---WARNING WARNING WARNING WARNING WARNING WARNING----"
+echo "------------------------------------------------------"
+echo "---nVidia driver build is enabled, please make sure---"
+echo "----that no process/container/VM uses the graphics----"
+echo "---otherwise the installation of the graphic driver---"
+echo "-------will fail and you are not able to use the------"
+echo "--------graphic card in any Docker container!---------"
+echo "------------------------------------------------------"
+echo "---WARNING WARNING WARNING WARNING WARNING WARNING----"
+echo "------------------------------------------------------"
 sleep 60
 
 if [ "${BUILD_DVB}" == "true" ]; then
