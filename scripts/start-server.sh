d0825450d34f60e906ee69942c5082fa39cf0ff1
ich777 <ich777@noreply.example.org>
2020-08-08 12:55:12 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -999,7 +999,10 @@ if [ "${BUILD_NVIDIA}" == "true" ]; then
 	git checkout v$LIBNVIDIA_CONTAINER_V
 	sed -i '/if (syscall(SYS_pivot_root, ".", ".") < 0)/,+1 d' ${DATA_DIR}/libnvidia-container/src/nvc_ldcache.c
 	sed -i '/if (umount2(".", MNT_DETACH) < 0)/,+1 d' ${DATA_DIR}/libnvidia-container/src/nvc_ldcache.c
-	DESTDIR=${DATA_DIR}/bzroot-extracted-$UNAME make install prefix=/usr
+	until DESTDIR=${DATA_DIR}/bzroot-extracted-$UNAME make install prefix=/usr; do
+		echo Something went wrong, retrying in 5 seconds...
+		sleep 5
+	done
 
 	## Create Docker daemon config file
 	mkdir -p ${DATA_DIR}/bzroot-extracted-$UNAME/etc/docker
@@ -1035,7 +1038,10 @@ EOF
 		### Compile 'nvidia-container-runtime' v3.1.4 and lower
 		echo "---Compiling 'nvidia-container-runtime', this can take some time, please wait!---"
 		cd ${DATA_DIR}/go/src/github.com/NVIDIA/nvidia-container-runtime/runtime/src
-		make
+		until make; do
+			echo Something went wrong, retrying in 5 seconds...
+			sleep 5
+		done
 		cp ${DATA_DIR}/go/src/github.com/NVIDIA/nvidia-container-runtime/runtime/src/nvidia-container-runtime ${DATA_DIR}/bzroot-extracted-$UNAME/usr/bin
 	else
 		### Compile 'nvidia-container-runtime' v3.2.0 and up
@@ -1055,7 +1061,10 @@ EOF
 		git clone https://github.com/NVIDIA/nvidia-container-toolkit
 		cd ${DATA_DIR}/go/src/github.com/NVIDIA/nvidia-container-toolkit
 		git checkout v$CONTAINER_TOOLKIT_V
-		make binary
+		until make binary; do
+			echo Something went wrong, retrying in 5 seconds...
+			sleep 5
+		done
 		cp ${DATA_DIR}/go/src/github.com/NVIDIA/nvidia-container-toolkit/nvidia-container-toolkit ${DATA_DIR}/bzroot-extracted-$UNAME/usr/bin
 		cd ${DATA_DIR}/bzroot-extracted-$UNAME/usr/bin
 		ln -s /usr/bin/nvidia-container-toolkit nvidia-container-runtime-hook
